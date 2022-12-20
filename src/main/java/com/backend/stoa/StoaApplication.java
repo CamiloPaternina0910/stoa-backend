67371dfe71bcfba942e3f2875ad0fb01754ec56f
Camilo <paterninapcamilo@gmail.com>
2022-12-20 15:12:48 -0500

    First commit

---
+++ src/main/java/com/backend/stoa/StoaApplication.java
@@ -0,0 +1,13 @@
+package com.backend.stoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/stoa/StoaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.stoa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
